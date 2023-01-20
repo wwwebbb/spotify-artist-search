624a878474b7e520b74b39c2616ab885db844a70
Miles <milesweldo@gmail.com>
2023-01-20 18:38:43 -0500

    http-errors package added

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 require('dotenv').config();
+const createError = require('http-errors');
 
 const app = express();
 
@@ -23,7 +24,7 @@ const btoa = (str) => {
   return Buffer.from(str).toString('base64');
 };
 
-app.get('/token', async (req, res) => {
+app.get('/token', async (req, res, next) => {
   try {
     const authHeader = btoa(`${CLIENT_ID}:${CLIENT_SECRET}`);
     const result = await axios.post(
@@ -37,10 +38,28 @@ app.get('/token', async (req, res) => {
       }
     );
     res.json(result.data);
-  } catch (error) {
-    console.log(error);
-    res.send(error);
+  } catch (err) {
+    console.log(err);
+    res.send(err);
+    next(err);
   }
+});
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  console.log(err);
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.json({ error: err });
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
