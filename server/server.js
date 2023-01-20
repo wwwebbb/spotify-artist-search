b5946acbc2ed73c6125c56a89ddaed20f68332a7
Miles <milesweldo@gmail.com>
2023-01-20 18:12:51 -0500

    port

--- server/server.js
+++ server/server.js
@@ -38,4 +38,4 @@ app.get('/', (req, res) => {
   res.send('Server is running!');
 });
 
-app.listen(8000, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
